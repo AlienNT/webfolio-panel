b40f896aff105beebe343b9ec9d551d5032a595b
Trysten M <nick.tristen22@gmail.com>
2024-04-17 22:59:34 +0300

    update(config & packages)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig(({ mode }) => {
+  console.log('mode', mode)
+  return {
+    plugins: [
+      vue()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      postcss: {
+        plugins: [autoprefixer]
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/css/variables/index.scss";`
+        }
+      }
+    }
+  }
 })
