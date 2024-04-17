6811d2c6f867dffe8ec25a879f9f121ec9b7ea3f
Trysten M <nick.tristen22@gmail.com>
2024-04-17 23:04:25 +0300

    update(components): create header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import Navigation from '@/components/navigation/Navigation.vue'
+import { useNavigation } from '@/store/useNavigation.ts'
+
+const { navigationList } = useNavigation()
+</script>
+
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="row">
+        <div class="col navigation-col">
+          <Navigation
+            :navigation-list="navigationList"
+          />
+        </div>
+        <div class="col logout-col">
+          <button
+            type="button"
+            class="logout-button"
+          >
+            logout
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  background: #2b2b2b;
+  .row {
+    align-items: center;
+  }
+}
+.logout-col {
+  flex: none;
+}
+</style>
