2accbc003e95461c6a639636e12479263441b299
Trysten M <nick.tristen22@gmail.com>
2024-04-17 23:04:55 +0300

    update(components/navigation): create navigation list component

---
+++ src/components/navigation/Navigation.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import NavigationItem from '@/components/navigation/NavigationItem.vue'
+
+interface NavigationProps {
+  navigationList: NavigationLink[]
+}
+
+defineProps<NavigationProps>()
+</script>
+
+<template>
+  <nav class="navigation">
+    <ul class="navigation-list">
+      <NavigationItem
+        v-for="(navigationItem, index) in navigationList"
+        :navigation-item="navigationItem"
+        :key="navigationItem._id || index"
+      />
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.navigation-list {
+  display: flex;
+  gap: 5px;
+}
+
+.navigation-list__item {
+  padding: 15px 5px;
+}
+</style>
