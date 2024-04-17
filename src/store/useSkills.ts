92c0145ee02023961029e563b71ae17ec523ece2
Trysten M <nick.tristen22@gmail.com>
2024-04-17 23:02:31 +0300

    add(store): add skills state

---
+++ src/store/useSkills.ts
@@ -0,0 +1,40 @@
+import { computed, reactive } from 'vue'
+import { getIndex } from '@/helpers'
+
+const state = reactive({
+  skills: <Skill[]>[]
+})
+
+export function useSkills() {
+  const skills = computed((): Skill[] => {
+    return state.skills
+  })
+  const skillGetter = (_id: Id) => computed((): Skill | undefined => {
+    return state.skills.find(skill => skill._id === _id)
+  })
+
+  function setSkills(skills: Skill[]) {
+    state.skills = skills
+  }
+
+  function setSkill(skill: Skill) {
+    if (!state.skills.length) {
+      return state.skills.push(skill)
+    }
+    const index = getIndex(state.skills, skill, '_id')
+
+    index > -1 ? state.skills[index] = skill : state.skills.push(skill)
+  }
+
+  async function fetchSkills() {
+    return ''
+  }
+
+  return {
+    skills,
+    skillGetter,
+    setSkills,
+    setSkill,
+    fetchSkills
+  }
+}
