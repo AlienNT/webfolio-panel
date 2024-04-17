5325ea0ace75a18605172d57c5bd0333fa6a6e91
Trysten M <nick.tristen22@gmail.com>
2024-04-17 23:06:12 +0300

    add(pages): create skill page

---
+++ src/pages/SkillsPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import PageTemplate from '@/templates/PageTemplate.vue'
+</script>
+
+<template>
+  <section class="section skills-section">
+    <PageTemplate>
+      <template v-slot:content>
+        skills page
+      </template>
+    </PageTemplate>
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
