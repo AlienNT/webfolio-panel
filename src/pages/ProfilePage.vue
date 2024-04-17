b7c36e7f22b25965c8feee1b01c60dbadf262dc2
Trysten M <nick.tristen22@gmail.com>
2024-04-17 23:06:02 +0300

    add(pages): create profile page

---
+++ src/pages/ProfilePage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import PageTemplate from '@/templates/PageTemplate.vue'
+</script>
+
+<template>
+  <section class="section profile-section">
+    <PageTemplate>
+      <template v-slot:content>
+        profile page
+      </template>
+    </PageTemplate>
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
