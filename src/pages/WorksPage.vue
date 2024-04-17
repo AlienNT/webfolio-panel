00c9727c7342901f59fb1bc3475937be0caf15a8
Trysten M <nick.tristen22@gmail.com>
2024-04-17 23:06:19 +0300

    add(pages): create work page

---
+++ src/pages/WorksPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import PageTemplate from '@/templates/PageTemplate.vue'
+</script>
+
+<template>
+  <section class="section works-section">
+    <PageTemplate>
+      <template v-slot:content>
+        works page
+      </template>
+    </PageTemplate>
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
