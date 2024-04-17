f71450bc3d273c11551cffd10461dba701757789
Trysten M <nick.tristen22@gmail.com>
2024-04-17 23:05:05 +0300

    update(components/navigation): create navigation item component

---
+++ src/components/navigation/NavigationItem.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+interface NavigationItemProps {
+  navigationItem: NavigationLink
+}
+
+defineProps<NavigationItemProps>()
+</script>
+
+<template>
+  <li class="navigation-list__item">
+    <router-link
+      active-class="active-link"
+      class="navigation-link"
+      :to="navigationItem.path"
+    >
+      {{ navigationItem.label || navigationItem.title }}
+    </router-link>
+  </li>
+</template>
+
+<style scoped lang="scss">
+.navigation-link {
+  transition: .2s ease;
+  color: #ccc;
+  cursor: pointer;
+  display: block;
+  font-size: 16px;
+
+  &:first-letter {
+    text-transform: uppercase;
+    font-family: monospace;
+  }
+}
+
+.active-link, .navigation-link:hover {
+  color: white;
+}
+</style>
