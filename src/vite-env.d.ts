726bef6f16848b4151b0922c10a5a437dd10be2e
Trysten M <nick.tristen22@gmail.com>
2024-04-17 23:03:22 +0300

    update(globalTypes): add global types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,47 @@
-/// <reference types="vite/client" />
+type Id = string
+
+interface DefaultInterface {
+  _id: Id
+}
+
+interface NavigationLink extends DefaultInterface {
+  path: string,
+  title: string
+  label?: string,
+}
+
+interface LanguageLocales {
+  ru: string,
+  ua: string,
+  en: string
+}
+
+interface FirstName extends LanguageLocales {
+}
+
+interface LastName extends LanguageLocales {
+}
+
+interface Profile extends DefaultInterface {
+  firstName: FirstName
+  lastName: LastName
+}
+
+interface Skill extends DefaultInterface {
+  title: string
+  active?: string
+  image: string
+}
+
+interface Work extends DefaultInterface {
+  image?: string,
+  path: string,
+  codePath: string,
+  chips?: string
+}
+
+interface Contact extends DefaultInterface {
+  title: string,
+  type: 'social' | 'email' | 'phone',
+  value: string | number,
+}
