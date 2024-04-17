9d4659421e716c4acd2e971bbafebd446aa029e5
Trysten M <nick.tristen22@gmail.com>
2024-04-17 23:05:53 +0300

    add(pages): create contact page

---
+++ src/pages/ContactsPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import PageTemplate from '@/templates/PageTemplate.vue'
+</script>
+
+<template>
+  <section class="section contacts-section">
+    <PageTemplate>
+      <template v-slot:content>
+        contacts page
+      </template>
+    </PageTemplate>
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
