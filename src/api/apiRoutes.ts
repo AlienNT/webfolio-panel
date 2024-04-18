b078326b8a99c5566ab081508459e68ed5a0b341
Trysten M <nick.tristen22@gmail.com>
2024-04-18 14:57:00 +0300

    update(api): update routes

--- src/api/apiRoutes.ts
+++ src/api/apiRoutes.ts
@@ -1,73 +1,65 @@
-type ApiPath = (_id: Id) => string
-
-interface ApiRoute {
-  method: 'GET' | 'POST' | 'PATCH' | 'DELETE',
-  path: string | ApiPath
-
-}
-
 export default {
   PROFILE: {
-    get: <ApiRoute>{
+    get: {
       method: 'GET',
       path: '/profile'
     },
-    update: <ApiRoute>{
+    update: {
       method: 'PATCH',
       path: '/profile'
     }
   },
   SKILLS: {
-    fetchAll: <ApiRoute>{
-      method: 'GET',
+    fetchAll: {
+      method: <Method>'GET',
       path: '/skills'
     },
-    create: <ApiRoute>{
-      method: 'POST',
+    create: {
+      method: <Method>'POST',
       path: '/skills'
     },
-    update: <ApiRoute>{
-      method: 'PATCH',
-      path: (_id: Id) => '/skills/' + _id
+    update: {
+      method: <Method>'PATCH',
+      path: (_id: Id): string => '/skills/' + _id
     },
-    delete: <ApiRoute>{
-      method: 'DELETE',
-      path: (_id: Id) => '/skills/' + _id
+    delete: {
+      method: <Method>'DELETE',
+      path: (_id: Id): string => '/skills/' + _id
     }
   },
   WORKS: {
-    fetchAll: <ApiRoute>{
-      method: 'GET',
+    fetchAll: {
+      method: <Method>'GET',
       path: '/works'
     },
-    create: <ApiRoute>{
-      method: 'POST',
+    create: {
+      method: <Method>'POST',
       path: '/works'
     },
-    update: <ApiRoute>{
-      method: 'PATCH',
+    update: {
+      method: <Method>'PATCH',
       path: (_id: Id) => '/works/' + _id
     },
-    delete: <ApiRoute>{
-      method: 'DELETE',
+    delete: {
+      method: <Method>'DELETE',
       path: (_id: Id) => '/works/' + _id
     }
   },
   CONTACTS: {
-    fetchAll: <ApiRoute>{
-      method: 'GET',
+    fetchAll: {
+      method: <Method>'GET',
       path: '/contacts'
     },
-    create: <ApiRoute>{
-      method: 'POST',
+    create: {
+      method: <Method>'POST',
       path: '/contacts'
     },
-    update: <ApiRoute>{
-      method: 'PATCH',
+    update: {
+      method: <Method>'PATCH',
       path: (_id: Id) => '/contacts/' + _id
     },
-    delete: <ApiRoute>{
-      method: 'DELETE',
+    delete: {
+      method: <Method>'DELETE',
       path: (_id: Id) => '/contacts/' + _id
     }
   }
