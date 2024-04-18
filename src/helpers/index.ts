fde875e2bb1dfee386a1e6eb86bba657eedc112b
Trysten M <nick.tristen22@gmail.com>
2024-04-18 15:01:19 +0300

    update(router): add redirect to auth page

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,12 @@
 export function getIndex<T>(array: T[], item: T, fieldName: keyof T): number {
   return array.findIndex(i => i[fieldName] === item[fieldName])
 }
+
+export function getIsAuth(): boolean {
+  const token = localStorage.getItem('token')
+  return !!token
+}
+
+export function removeToken() {
+  localStorage.removeItem('token')
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { getIsAuth } from '@/helpers'
+
 import { routes } from './routes.ts'
 
 const router = createRouter({
   routes,
   history: createWebHistory()
+})
+
+router.beforeEach((to) => {
+  const isAuth = getIsAuth()
+
+  if (isAuth) return true
+
+  // if (!isAuth && from.name === 'auth') {
+  //   return false
+  // }
+  if (!isAuth && to.name !== 'auth') {
+    return { name: 'auth' }
+  }
 })
 
 export default router
