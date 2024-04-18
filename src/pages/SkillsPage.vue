a51edbcb4b3445b82348a9a7b57938e800156c11
Trysten M <nick.tristen22@gmail.com>
2024-04-18 15:00:33 +0300

    update(pages): add skills to skill page

--- src/pages/SkillsPage.vue
+++ src/pages/SkillsPage.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
 
 import PageTemplate from '@/templates/PageTemplate.vue'
+import SkillsList from '@/components/skills/SkillsList.vue'
+
+import { useSkills } from '@/store/useSkills.ts'
+import { onMounted } from 'vue'
+import { useSkillRequests } from '@/api/requests/useSkillRequests.ts'
+
+const { skills, setSkills } = useSkills()
+const { fetchSkills } = useSkillRequests()
+const { request } = fetchSkills({})
+
+onMounted(() => {
+  if (!skills.value.length) {
+    request().then((res) => {
+      setSkills(res)
+    })
+  }
+})
 </script>
 
 <template>
   <section class="section skills-section">
     <PageTemplate>
       <template v-slot:content>
-        skills page
+        <SkillsList
+          :skills="skills"
+        />
       </template>
     </PageTemplate>
   </section>
