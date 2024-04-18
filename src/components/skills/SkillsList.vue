c852f525aabd0499daa37e4a79febd947fd42524
Trysten M <nick.tristen22@gmail.com>
2024-04-18 14:57:44 +0300

    add(components/skills): add skill list component

---
+++ src/components/skills/SkillsList.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+
+import SkillsListItem from '@/components/skills/SkillsListItem.vue'
+
+interface SkillsProps {
+  skills: Skill[]
+}
+
+defineProps<SkillsProps>()
+</script>
+
+<template>
+  <ul class="skills-list">
+    <SkillsListItem
+      v-for="skill in skills"
+      :key="skill._id"
+      :skill="skill"
+    />
+  </ul>
+</template>
+
+<style scoped lang="scss">
+.skills-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
+  justify-content: center;
+}
+</style>
