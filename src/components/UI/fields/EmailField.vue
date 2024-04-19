51f415c95b6ceac4796b7752790b73ea6ad1699c
Trysten M <nick.tristen22@gmail.com>
2024-04-19 19:39:52 +0300

    add(components): add email field component

---
+++ src/components/UI/fields/EmailField.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+import VInput from '@/components/UI/VInput.vue'
+import { Input } from '@/types/fieldTypes.ts'
+
+import { fieldEmits } from '@/helpers/fieldEmits.ts'
+
+interface EmailFieldProps extends Omit<Input, 'type'> {
+}
+
+withDefaults(defineProps<EmailFieldProps>(), {
+  placeholder: 'email'
+})
+const emit = defineEmits(fieldEmits)
+</script>
+
+<template>
+  <label class="label">
+    <VInput
+      type="email"
+      :value="value"
+      :disabled="disabled"
+      :readonly="readonly"
+      :placeholder="placeholder"
+      @on-focus="(e) => emit('onFocus', e)"
+      @on-blur="(e) => emit('onBlur', e)"
+      @on-input="(e) => emit('onInput', e)"
+      @on-change="(e) => emit('onChange', e)"
+    />
+  </label>
+</template>
