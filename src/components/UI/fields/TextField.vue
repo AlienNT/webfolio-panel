9e5a1464310753c7a05d9aa4a84437ec82b402eb
Trysten M <nick.tristen22@gmail.com>
2024-04-19 19:41:10 +0300

    add(components): add text field component

---
+++ src/components/UI/fields/TextField.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+import VInput from '@/components/UI/VInput.vue'
+import { Input } from '@/types/fieldTypes.ts'
+
+import { fieldEmits } from '@/helpers/fieldEmits.ts'
+
+interface TextFieldProps extends Omit<Input, 'type'> {
+}
+
+defineProps<TextFieldProps>()
+const emit = defineEmits(fieldEmits)
+</script>
+
+<template>
+  <label class="label">
+    <VInput
+      type="text"
+      :value="value"
+      :disabled="disabled"
+      :readonly="readonly"
+      :placeholder="placeholder"
+      @on-focus="(e) => emit('onFocus', e)"
+      @on-blur="(e) => emit('onBlur', e)"
+      @on-input="(e) => emit('onInput', e)"
+      @on-change="(e) => emit('onChange', e)"
+    />
+  </label>
+</template>
