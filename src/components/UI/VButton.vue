cb4b39ddb1d1e4b2cba05f0372226a23aa8dd054
Trysten M <nick.tristen22@gmail.com>
2024-04-19 19:42:19 +0300

    add(components): add button component

---
+++ src/components/UI/VButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+interface ButtonProps {
+  type?: 'button' | 'submit',
+  title?: string,
+  label?: string,
+  disabled?: boolean
+}
+
+withDefaults(defineProps<ButtonProps>(), {
+  label: 'button',
+  title: 'button',
+  type: 'button',
+  disabled: false
+})
+
+const emit = defineEmits<{
+  (e: 'onClick'): void
+}>()
+</script>
+
+<template>
+  <button
+    class="button"
+    :type="type"
+    :title="title"
+    :disabled="disabled"
+    @click="emit('onClick')"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+
+</style>
