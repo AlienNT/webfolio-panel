7dfc5e38550269b89714772798ac330ff03dd385
Trysten M <nick.tristen22@gmail.com>
2024-04-19 19:40:34 +0300

    add(components): add password field component

---
+++ src/components/UI/fields/PasswordField.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+import VInput from '@/components/UI/VInput.vue'
+import { Input } from '@/types/fieldTypes.ts'
+
+import { fieldEmits } from '@/helpers/fieldEmits.ts'
+
+interface PasswordFieldProps extends Omit<Input, 'type'> {
+}
+
+withDefaults(defineProps<PasswordFieldProps>(), {
+  placeholder: 'password'
+})
+
+const emit = defineEmits(fieldEmits)
+</script>
+
+<template>
+  <label class="label">
+    <VInput
+      type="password"
+      :value="value"
+      :disabled="disabled"
+      :readonly="readonly"
+      :placeholder="placeholder"
+      @on-focus="(e) => emit('onFocus', e)"
+      @on-blur="(e) => emit('onBlur', e)"
+      @on-input="(e) => emit('onInput', e)"
+      @on-change="(e) => emit('onChange', e)"
+    />
+  </label>
+</template>
