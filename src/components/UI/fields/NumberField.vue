52cee5e3381090148b8662a8fbdd2e15d872c4c9
Trysten M <nick.tristen22@gmail.com>
2024-04-19 19:40:15 +0300

    add(components): add number field component

---
+++ src/components/UI/fields/NumberField.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { fieldEmits } from '@/helpers/fieldEmits.ts'
+
+import VInput from '@/components/UI/VInput.vue'
+import { Input } from '@/types/fieldTypes.ts'
+
+interface NumberFieldProps extends Omit<Input, 'type'>{
+}
+
+defineProps<NumberFieldProps>()
+
+const emit = defineEmits(fieldEmits)
+</script>
+
+<template>
+  <label class="label">
+    <VInput
+      type="email"
+      :value="value"
+      :disabled="disabled"
+      :readonly="readonly"
+      :placeholder="placeholder"
+      @on-focus="(e) => emit('onFocus', e)"
+      @on-blur="(e) => emit('onBlur', e)"
+      @on-input="(e) => emit('onInput', e)"
+      @on-change="(e) => emit('onChange', e)"
+    />
+  </label>
+</template>
