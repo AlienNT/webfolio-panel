de3c797f883729b03edfc31cba241645beb9234b
Trysten M <nick.tristen22@gmail.com>
2024-04-19 20:21:09 +0300

    update(pages): add contacts to contact page

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -1,13 +1,36 @@
 <script setup lang="ts">
 
 import PageTemplate from '@/templates/PageTemplate.vue'
+import Loader from '@/components/UI/Loader.vue'
+
+import { useContacts } from '@/store/useContacts.ts'
+import { useContactsRequest } from '@/api/requests/useContactsRequest.ts'
+import { onMounted } from 'vue'
+
+const { fetchContacts } = useContactsRequest()
+const { isLoad, request } = fetchContacts({})
+const { contacts, setContacts } = useContacts()
+
+onMounted(() => {
+  if (!contacts.value.length) {
+    request().then((res) => {
+      setContacts(res)
+    })
+  }
+})
 </script>
 
 <template>
   <section class="section contacts-section">
     <PageTemplate>
       <template v-slot:content>
-        contacts page
+        contacts
+        <div v-if="contacts.length" class="contacts-list">
+          {{ contacts }}
+        </div>
+        <Loader
+          v-if="isLoad"
+        />
       </template>
     </PageTemplate>
   </section>
