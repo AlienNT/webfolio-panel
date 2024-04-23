07bbd30f237b3b2e6633a1f6e428416d01f73538
Trysten M <nick.tristen22@gmail.com>
2024-04-23 16:56:48 +0300

    update(configs): update api url config

--- src/configs/apiConfig.ts
+++ src/configs/apiConfig.ts
@@ -1,3 +1,5 @@
 export default {
-  API_URL: import.meta.env.VITE_API_URL || 'http://localhost:5000/aliennt/api'
+  API_URL: import.meta.env.VITE_API_URL && import.meta.env.MODE !== 'development' ?
+    import.meta.env.VITE_API_URL :
+    'http://localhost:5000/aliennt/api'
 }
