6dc31a6afc2d76315cbe2fc80b2535842b9dc37f
Trysten M <nick.tristen22@gmail.com>
2024-04-23 16:58:12 +0300

    update(helpers): add setCssVar method

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -11,3 +11,13 @@ export function checkTokenExpire(token: Token) {
   const decode = JSON.parse(atob(token.split('.')[1]))
   return decode.exp * 1000 < new Date().getTime()
 }
+
+interface SetProperty {
+  name: string,
+  value: string | number,
+  unit?: 'px' | '%' | null
+}
+
+export function setCssVar({ name, value, unit = 'px' }: SetProperty) {
+  document.documentElement.style.setProperty(name, value + (unit || ''))
+}
