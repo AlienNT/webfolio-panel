bfe521cbcae83e1414d961fab02d3f44498b3526
Trysten M <nick.tristen22@gmail.com>
2024-04-23 23:06:02 +0300

    add(contacts): add contact list component

---
+++ src/components/contacts/ContactsList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import ContactsListItem from '@/components/contacts/ContactsListItem.vue'
+
+interface ContactsProps {
+  contacts: Contact[]
+}
+
+defineProps<ContactsProps>()
+</script>
+<template>
+  <ul class="contacts-list">
+    <ContactsListItem
+      v-for="contact in contacts"
+      :key="contact._id"
+      :contact="contact"
+    />
+  </ul>
+</template>
+
+<style scoped lang="scss">
+.contacts-list {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+</style>
