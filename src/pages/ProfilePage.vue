9814b50ba2b55832069c8f46903db962a4a39367
Trysten M <nick.tristen22@gmail.com>
2024-05-01 21:11:51 +0300

    update(pages): add fetch profile & show profile data in ProfilePage.vue

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
 
 import PageTemplate from '@/templates/PageTemplate.vue'
+
+import { onMounted } from 'vue'
+import { useProfile } from '@/store/useProfile.ts'
+import { useProfileRequest } from '@/api/requests/useProfileRequest.ts'
+
+const { profile, setProfile } = useProfile()
+const { fetchProfile } = useProfileRequest()
+
+
+onMounted(() => {
+  if (!profile.value || Object.keys(!profile.value)) {
+    fetchProfile({}).request().then(res => {
+      setProfile(res)
+    })
+  }
+})
 </script>
 
 <template>
   <section class="section profile-section">
     <PageTemplate>
       <template v-slot:content>
-        profile page
+        <div>
+          profile page
+        </div>
+        {{profile}}
       </template>
     </PageTemplate>
   </section>
